07ff27c1dbd64b1595e6a45b00b9121c24b49156
Erick Chavez <erickchavezescobar@gmail.com>
2024-12-28 16:39:35 -0600

    input validation and word count bug fixed

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ const charsCountEl = document.querySelector('.word_characters--counter')
 const XCountEl = document.querySelector('.word_x--counter')
 const fbCountEl = document.querySelector('.word_facebook--counter')
 
-//Select the element which will receive the input & create a fn to react
-textareaEl.addEventListener('input', function () {
+const inputHandler = () => {
+  //input validation
+  if (textareaEl.value.includes('<script>')) {
+    alert("You can't type JS code!")
+    textareaEl.value = textareaEl.value.replace('<script>', '')
+  }
   //determine new numbers
+  let numberOfWords = textareaEl.value.split(' ').length
+  if (textareaEl.value.length === 0) {
+    numberOfWords = 0
+  }
   const numberOfChars = textareaEl.value.length
   const xCharsLeft = 200 - numberOfChars
   const fbCharsLeft = 2200 - numberOfChars
@@ -20,7 +28,11 @@ textareaEl.addEventListener('input', function () {
     ? fbCountEl.classList.add('limit')
     : fbCountEl.classList.remove('limit')
   //set new numbers
+  wordsCountEl.textContent = numberOfWords
   charsCountEl.textContent = numberOfChars
   XCountEl.textContent = xCharsLeft
   fbCountEl.textContent = fbCharsLeft
-})
+}
+
+//Select the element which will receive the input & create a fn to react
+textareaEl.addEventListener('input', inputHandler)
